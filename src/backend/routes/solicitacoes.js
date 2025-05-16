7bace1f331282f3f56c65e1bebd0e76acf98857d
Vinicius Piras <viniciuspigo@gmail.com>
2025-05-15 22:20:05 -0300

    Atualizar .gitignore para ignorar node_modules e .env

--- src/backend/routes/solicitacoes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const solicitacaoController = require('../controllers/solicitacaoController');
-
-router.post('/', solicitacaoController.createSolicitacao);
-router.get('/', solicitacaoController.getAllSolicitacoes);
-
-module.exports = router;
--- src/backend/routes/usuarios.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const usuarioController = require('../controllers/usuarioController');
-
-router.post('/register', usuarioController.registerUser);
-router.get('/create-admin', usuarioController.createAdmin);
-router.post('/login', usuarioController.loginUser);
-
-module.exports = router;
